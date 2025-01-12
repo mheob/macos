9ccc47d4ebd3f1df491024f656a7f1a9dcf021c3
Alex Böhm <tools@boehm.work>
2025-01-12 09:30:33 +0100

    feat: update nvim settings

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -2,8 +2,12 @@
 vim.g.mapleader = " "
 
 -- movement
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move current selected line(s) up
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move current selected line(s) down
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv") -- move current selected line(s) up
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv") -- move current selected line(s) down
+vim.keymap.set("n", "<leader>h", "<C-w>h")          -- navigate to the split window on the immediate left
+vim.keymap.set("n", "<leader>j", "<C-w>j")          -- navigate to the split window below
+vim.keymap.set("n", "<leader>k", "<C-w>k")          -- navigate to the split window top
+vim.keymap.set("n", "<leader>l", "<C-w>l")          -- navigate to the split window on the immediate right
 
 -- buffers
 vim.keymap.set("n", "<leader>n", ":bn<cr>")
